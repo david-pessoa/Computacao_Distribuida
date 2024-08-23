4d0c03e83b1d7d17687ca7dc6becb9e9e18558a6
david-pessoa <pessoa.dvlbp@gmail.com>
2024-08-23 17:34:45 -0300

    tarefa realizada

--- program.c
+++ program.c
@@ -9,13 +9,38 @@ typedef struct objeto
     float altura;
 }objeto;
 
+void add_pessoa(FILE* arquivo, objeto ob[], int i)
+{
+    fscanf(arquivo, "%s", ob[i].nome);
+    fscanf(arquivo, "%d", &ob[i].idade);
+    fscanf(arquivo, "%f", &ob[i].altura);
+}
+
+char* compara(char a[], char b[])
+{
+  int num = strcmp(a, b);
+    return (int*) num;
+}
+
+void sort_objetos(objeto ob[], int n)
+{
+    qsort(ob, n, sizeof(objeto), compara);
+}
+
+void show_data(objeto ob[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("\nNome: %s\n", ob[i].nome);
+        printf("Idade: %d\n", ob[i].idade);
+        printf("Altura: %.1f\n", ob[i].altura);
+    }
+}
+
 int main()
 {
     objeto objetos[10];
     FILE* arquivo;
-    char linha[256];
-    char *conteudo = NULL;
-    size_t tamanho = 0;
     int i = 0;
 
     arquivo = fopen("dados.txt", "r");
@@ -25,34 +50,15 @@ int main()
         return 1;
     }
 
-    while(fscanf(arquivo, "%s %d %f", objetos[i].nome, &objetos[i].idade, &objetos[i].altura) == 3)
+    while(!feof(arquivo))
     {
-        if (fgets(objetos[i].nome, sizeof(objetos[i].nome), arquivo) == NULL)
-            break;
-        objetos[i].nome[strcspn(objetos[i].nome, "\n")] = '\0'; // Remove o newline
-
-        // Leitura da idade (2ª linha)
-        if (fscanf(arquivo, "%d", &objetos[i].idade) != 1)
-            break;
-
-        // Leitura da altura (3ª linha)
-        if (fscanf(arquivo, "%f", &objetos[i].altura) != 1)
-            break;
-
-        // Ignora o newline após a altura
-        fgetc(arquivo);
-        printf("%s\n", objetos[i].nome);
+        add_pessoa(arquivo, objetos, i);
         i++;
     }
     fclose(arquivo);
 
-    for(int j = 0; j < i; j++)
-    {
-        printf("\nNome: %s\n", objetos[i].nome);
-        printf("Idade: %d\n", objetos[i].idade);
-        printf("Altura: %f\n", objetos[i].altura);
-    }
-    free(conteudo);
+    sort_objetos(objetos, i);
+    show_data(objetos, i);
     return 0;
 
 }
