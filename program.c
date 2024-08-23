9495608278aeb1e58fe5aff2cac2df7569e3bc33
david-pessoa <pessoa.dvlbp@gmail.com>
2024-08-23 17:48:39 -0300

    tarefa finalizada e comentada

--- program.c
+++ program.c
@@ -2,32 +2,36 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct objeto
+//Integrantes:
+//David Pessoa  10402647
+//João Pedro de Souza Costa Ferreira  10400720
+
+typedef struct objeto //cria struct objeto com nome, idade e altura
 {
     char nome[50];
     int idade;
     float altura;
 }objeto;
 
-void add_pessoa(FILE* arquivo, objeto ob[], int i)
+void add_pessoa(FILE* arquivo, objeto ob[], int i) //lê os dados no arquivo e add a pessoa no vetor objetos
 {
     fscanf(arquivo, "%s", ob[i].nome);
     fscanf(arquivo, "%d", &ob[i].idade);
     fscanf(arquivo, "%f", &ob[i].altura);
 }
 
-char* compara(char a[], char b[])
+int* compara(char a[], char b[]) //Compara duas strings com strcmp()
 {
   int num = strcmp(a, b);
     return (int*) num;
 }
 
-void sort_objetos(objeto ob[], int n)
+void sort_objetos(objeto ob[], int n) //Ordena os objetos no vetor na ordem alfabética dos nomes
 {
     qsort(ob, n, sizeof(objeto), compara);
 }
 
-void show_data(objeto ob[], int n)
+void show_data(objeto ob[], int n) //Imprime os dados do vetor
 {
     for(int i = 0; i < n; i++)
     {
@@ -39,9 +43,9 @@ void show_data(objeto ob[], int n)
 
 int main()
 {
-    objeto objetos[10];
-    FILE* arquivo;
-    int i = 0;
+    objeto objetos[10]; //Vetor de objetos
+    FILE* arquivo; //arquivo lido
+    int i = 0; // variável contadora de pessoas no arquivo
 
     arquivo = fopen("dados.txt", "r");
     if(arquivo == NULL)
@@ -52,12 +56,12 @@ int main()
 
     while(!feof(arquivo))
     {
-        add_pessoa(arquivo, objetos, i);
+        add_pessoa(arquivo, objetos, i); //Add as pessoas e seus dados no vetor
         i++;
     }
     fclose(arquivo);
 
-    sort_objetos(objetos, i);
+    sort_objetos(objetos, i); //Ordena e exibe o vetor
     show_data(objetos, i);
     return 0;
 
